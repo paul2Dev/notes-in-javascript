646a9bd946cb20f5a1b1c7fad6cfce3b1eb13fcd
Paul2D <paul.maxineanu@gmail.com>
2022-11-15 17:28:03 +0200

    create NotesApi

---
+++ js/NotesAPI.js
@@ -0,0 +1,34 @@
+export default class NotesAPI{
+    static getAllNotes(){
+        const notes = JSON.parse(localStorage.getItem("notesapp-notes") || "[]")
+        
+        return notes.sort((a, b) => {
+            return new Date(a.updated) > new Date(b.updated) ? -1 : 1
+        })
+    }
+    
+    static saveNote(newNote){
+        const notes = this.getAllNotes()
+
+        const existingNote = notes.find(note => note.id === newNote.id)
+
+        if(existingNote){
+            existingNote.title = newNote.title
+            existingNote.body = newNote.body
+            existingNote.updated = new Date().toISOString()
+        } else {
+            newNote.id = Math.floor(Math.random() * 1000000000)
+            newNote.updated = new Date().toISOString()
+
+            notes.push(newNote)
+        }  
+
+        localStorage.setItem("notesapp-notes", JSON.stringify(notes))
+    }
+
+    static deleteNote(id){
+        const notes = this.getAllNotes()
+        const newNotes = notes.filter(note => note.id !== id)
+        localStorage.setItem("notesapp-notes", JSON.stringify(newNotes))
+    }
+}
---
+++ js/main.js
@@ -0,0 +1,19 @@
+import NotesAPI from './NotesAPI';
+
+//save note
+//NotesAPI.saveNote({
+//     title: 'My new note',
+//     body: 'body test'
+// })
+
+//update note
+// NotesAPI.saveNote({
+//     id: 943343421,
+//     title: 'My new note - modified',
+//     body: 'body test - modified'
+// })
+
+//delete note 
+//NotesAPI.deleteNote(461234215)
+
+console.log(NotesAPI.getAllNotes());
